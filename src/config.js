202b5f5fd6084a15ea3db9d7623cc5a9edae3a11
Mohit Singh Chauhan <mohit.chauhan2580@gmail.com>
2023-04-06 22:40:26 +0530

    Changed Code Acc to Swiggy's API

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ export const IMAGE_CDN_URL =
 export const CROUSEL_IMAGE_CDN_URL =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_520,h_520/";
 export const RESTAURANT_MENU_URL =
-  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=27.20534&lng=78.045887&restaurantId=";
+  "https://food-villa-server.vercel.app/api/restaurant/menu?lat=27.8973944&lng=78.0880129&menuId=";
 export const SWIGGY_RESTAURANT_CDN_URL = 
 "https://food-villa-server.vercel.app/api/restaurants?lat=27.8973944&lng=78.0880129&offset=15&sortBy=RELEVANCE&pageType=SEE_ALL&page_type=DESKTOP_SEE_ALL_LISTING"
 
